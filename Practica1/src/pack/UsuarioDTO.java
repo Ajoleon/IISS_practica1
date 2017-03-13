be22bacdbf09e992fe16c1c2e8ac9a638f884d10
DanielMesaGonzalez <dmg00040@red.ujaen.es>
2017-03-13 12:06:55 +0100

    getters and setters DTO

--- Practica1/src/pack/UsuarioDTO.java
+++ Practica1/src/pack/UsuarioDTO.java
@@ -12,7 +12,30 @@ public class UsuarioDTO {
 		this.dir = dir;
 		this.tlf = tlf;
 	}
-		
 	
+	public String getNombre( ){
+	    return this.user;
+	  }
+	public void setNombre(String user){
+	    this.user = user;
+	  }
+	public String getEmail( ){
+	    return this.email;
+	  }
+	public void setEmail(String email){
+	    this.email = email;
+	  }	
+	public String getDireccion( ){
+	    return this.dir;
+	  }
+	public void setDireccion(String dir){
+	    this.dir = dir;
+	  }
+	public String getTelefono( ){
+	    return this.tlf;
+	  }
+	public void setTelefono(String tlf){
+	    this.tlf = tlf;
+	  }
 	
 }
